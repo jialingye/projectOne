c2bd5e1aff082b69bbdd32529f6591d92b115e2f
Jialing Ye <jialingye@Jialings-MacBook-Pro.local>
2023-02-19 23:49:22 -0600

    add pictures and add CSS and JS

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
-
+const objectsToFind=[
+{
+picName:'Hogwarts Train',
+pictureLink: './images/train.png',
+alt: 'train',
+itemPicture: ['./image/train/owl.png','./image/train/luggage.png','./image/train/map.png','./image/train/wand.png','./image/train/frog.png '  ],
+itemAlt:['owl','luggage','map','wand','frog'],
+itemLocation: [{id:1,top:'100px',left:'200px'},{id:2,top:'100px',left:'200px'},{id:3,top:'100px',left:'200px'},{id:4,top:'100px',left:'200px'},{id:5,top:'100px',left:'200px'}]
+}, 
+{
+picName:'Potions Class',
+pictureLink: './images/potions.png',
+alt: 'potions',
+itemPicture: ['./image/potions/tank.png','./image/potions/bottle.png','./image/potions/lizard.png','./image/potions/knift.png','./image/potions/book.png '  ],
+itemAlt:['tank','bottle','lizard','knift','book'],
+itemLocation: [{id:1,top:'100px',left:'200px'},{id:2,top:'100px',left:'200px'},{id:3,top:'100px',left:'200px'},{id:4,top:'100px',left:'200px'},{id:5,top:'100px',left:'200px'}]
+}, 
+{
+picName:'Ravenclaw Common Room',
+pictureLink: './images/ravenclaw.png',
+alt: 'ravenclaw',
+itemPicture: ['./image/ravenclaw/telescope.png','./image/ravenclaw/bottle.png','./image/ravenclaw/glass.png','./image/ravenclaw/book.png','./image/ravenclaw/chair.png '  ],
+itemAlt:['telescope','bottle','glass','book','chair'],
+itemLocation: [{id:1,top:'100px',left:'200px'},{id:2,top:'100px',left:'200px'},{id:3,top:'100px',left:'200px'},{id:4,top:'100px',left:'200px'},{id:5,top:'100px',left:'200px'}]
+}, 
+{
+picName:'Hufflepuff Common Room',
+pictureLink: './images/hufflepuff.png',
+alt: 'hufflepuff',
+itemPicture: ['./image/hufflepuff/plant.png','./image/hufflepuff/machine.png','./image/hufflepuff/fire.png','./image/hufflepuff/people.png','./image/hufflepuff/frog.png '  ],
+itemAlt:['plant','machine','fire','people','frog'],
+itemLocation: [{id:1,top:'100px',left:'200px'},{id:2,top:'100px',left:'200px'},{id:3,top:'100px',left:'200px'},{id:4,top:'100px',left:'200px'},{id:5,top:'100px',left:'200px'}]
+},
+{
+picName:'Slytherin Common Room',
+pictureLink: './images/slytherin.png',
+alt: 'slytherin',
+itemPicture: ['./image/slytherin/skull.png','./image/slytherin/gift.png','./image/slytherin/candle.png','./image/slytherin/cushion.png','./image/slytherin/voldemort.png '  ],
+itemAlt:['skull','gift','candle','cushion','voldemort'],
+itemLocation: [{id:1,top:'100px',left:'200px'},{id:2,top:'100px',left:'200px'},{id:3,top:'100px',left:'200px'},{id:4,top:'100px',left:'200px'},{id:5,top:'100px',left:'200px'}]
+},
+{
+picName:'Gryffindor Common Room',
+pictureLink: './images/gryffindor.png',
+alt: 'gryffindor',
+itemPicture: ['./image/gryffindor/basket.png','./image/gryffindor/scarf.png','./image/gryffindor/painting.png','./image/gryffindor/lamp.png','./image/gryffindor/mug.png '  ],
+itemAlt:['basket','scarf','painting','lamp','mug'],
+itemLocation: [{id:1,top:'100px',left:'200px'},{id:2,top:'100px',left:'200px'},{id:3,top:'100px',left:'200px'},{id:4,top:'100px',left:'200px'},{id:5,top:'100px',left:'200px'}]
+}
+];
 
 
